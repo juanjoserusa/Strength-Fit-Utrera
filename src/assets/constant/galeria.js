b3a5fff243c550b7cc1e03916fd99d4c771e65e9
mcaruisan <mcarmen.rs79@gmail.com>
2024-08-04 18:37:14 +0200

    fix galery mistakes

--- src/assets/constant/galeria.js
+++ src/assets/constant/galeria.js
@@ -4,79 +4,79 @@ export const galeria = [
         nombre:"Multiestación 2 torres"
     },
     { 
-        image:"./src/assets/galeriaImages/cintaDeCorrer",
+        image:"./src/assets/galeriaImages/cintaDeCorrer.jpg",
         nombre:"Cinta de correr"
     }, 
     {
-        image:"./src/assets/galeriaImages/bicicletaEliptica",
+        image:"./src/assets/galeriaImages/bicicletaEliptica.jpg",
         nombre:"Bicicleta elíptica"
     },
     {
-        image:"./src/assets/galeriaImages/bicicletaSpinning",
+        image:"./src/assets/galeriaImages/bicicletaSpinning.jpg",
         nombre:"Bicicleta de spinning"
     },
     {
-        image:"./src/assets/galeriaImages/hipThrust",
+        image:"./src/assets/galeriaImages/hipThrust.jpg",
         nombre:"Hip thrust"
     },
     {
-        image:"./src/assets/galeriaImages/powerRack",
+        image:"./src/assets/galeriaImages/powerRack.jpg",
         nombre:"Power rack"
     },
     {
-        image:"./src/assets/galeriaImages/squatRack",
+        image:"./src/assets/galeriaImages/squatRack.jpg",
         nombre:"Rack multifuncional"
     },
     {
-        image:"./src/assets/galeriaImages/bancoOlimpicoMultifuncion",
+        image:"./src/assets/galeriaImages/bancoOlimpicoMultifuncion.jpg",
         nombre:"Banco olímpico multifunción"
     },
     {
-        image:"./src/assets/galeriaImages/pecFly",
+        image:"./src/assets/galeriaImages/pecFly.jpg",
         nombre:"Pec fly"
     },
     {
-        image:"./src/assets/galeriaImages/pressHombroPecho",
+        image:"./src/assets/galeriaImages/pressHombroPecho.jpg",
         nombre:"Press hombro y pecho"
     },
     {
-        image:"./src/assets/galeriaImages/fondosTriceps",
+        image:"./src/assets/galeriaImages/fondosTriceps.jpg",
         nombre:"Fondos tríceps"
     },
     {
-        image:"./src/assets/galeriaImages/remo",
+        image:"./src/assets/galeriaImages/remo.jpg",
         nombre:"Remo"
     },
     {
-        image:"./src/assets/galeriaImages/bicepsTriceps",
+        image:"./src/assets/galeriaImages/bicepsTriceps.jpg",
         nombre:"Bíceps-Tríceps"
     },
     {
-        image:"./src/assets/galeriaImages/elevacionPiernasFemoralTumbado",
+        image:"./src/assets/galeriaImages/elevacionPiernasFemoralTumbado.jpg",
         nombre:"Elevación de piernas y femoral tumbado"
     },
     {
-        image:"./src/assets/galeriaImages/prensaYJaca",
+        image:"./src/assets/galeriaImages/prensaYJaca.jpg",
         nombre:"Prensa y jaca"
     },
     {
-        image:"./src/assets/galeriaImages/bancoAbdominal",
+        image:"./src/assets/galeriaImages/bancoAbdominal.jpg",
         nombre:"Banco abdominal"
     },
     {
-        image:"./src/assets/galeriaImages/bancoLumbar",
+        image:"./src/assets/galeriaImages/bancoLumbar.jpg",
         nombre:"Banco lumbar"
     },
     {
-        image:"./src/assets/galeriaImages/bancoAjustable",
+        image:"./src/assets/galeriaImages/bancoAjustable.jpg",
         nombre:"Banco ajustable"
     },
     {
-        image:"./src/assets/galeriaImages/extensionYFemoralSentado",
+        image:"./src/assets/galeriaImages/extensionYFemoralSentado.jpg",
         nombre:"Extensión y femoral sentado"
     },
     {
-        image:"./src/assets/galeriaImages/abductorAdductor",
+        image:"./src/assets/galeriaImages/abductorAdductor.jpg",
         nombre:"Abductor-Adductor"
     }
 ];
