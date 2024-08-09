12b8105790487da4bfb8f9ae1d860e51f406a505
mcaruisan <mcarmen.rs79@gmail.com>
2024-08-09 17:18:49 +0200

    tarifas y promoción

--- src/assets/constant/tarifas.js
+++ src/assets/constant/tarifas.js
@@ -1,42 +1,20 @@
 export const tarifas = [
     {
-        nombre: "Tarifa Pre StrengthFit",
-        descripcion: [
-            "Exclusivo si reservas antes del 25 de Agosto de 2024",
-            "Acceso ilimitado al gimnasio",
-            "Entrenamiento básico a elección",
-            "Acceso a duchas y vestuarios"
-        ],
-        precio: 20.00,
-        link: "https://tugimnasio.com/tarifa-basica"
-    },
-    {
         nombre: "Tarifa PowerStrength",
         descripcion: [
-            "Acceso ilimitado al gimnasio",
-            "Entrenamiento básico a elección",
-            "Acceso a duchas y vestuarios"
+            "Acceso ilimitado al gimnasio.",
+            "Entrenamiento básico a elección.",
+            "Acceso a duchas y vestuarios."
         ],
         precio: 25.00,
         link: "https://tugimnasio.com/tarifa-premium"
     },
     {
-        nombre: "Tarifa Pre SportStrength",
-        descripcion: [
-            "Exclusivo si reservas antes del 25 de Agosto de 2024",
-            "Acceso limitado al gimnasio de dos días por semana",
-            "Entrenamiento básico a elección",
-            "Acceso a duchas y vestuarios"
-        ],
-        precio: 10.00,
-        link: "https://tugimnasio.com/tarifa-estudiante"
-    },
-    {
         nombre: "Tarifa SportStrength",
         descripcion: [
-            "Acceso limitado al gimnasio de dos días por semana",
-            "Entrenamiento básico a elección",
-            "Acceso a duchas y vestuarios"
+            "Acceso limitado al gimnasio de dos días por semana.",
+            "Entrenamiento básico a elección.",
+            "Acceso a duchas y vestuarios."
         ],
         precio: 15.00,
         link: "https://tugimnasio.com/tarifa-estudiante"
